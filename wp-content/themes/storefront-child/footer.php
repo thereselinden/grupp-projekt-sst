e1f8f83f265f2c2d8c52c7e4faa4d253ac0048d7
Therese Linden <theresemlinden@gmail.com>
2023-04-26 15:29:14 +0200

    Remove header footer element on checkout page

---
+++ wp-content/themes/storefront-child/footer.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package storefront
+ */
+
+?>
+
+</div><!-- .col-full -->
+</div><!-- #content -->
+
+<?php do_action('storefront_before_footer'); ?>
+
+<?php if (!is_checkout()) { ?>
+
+  <footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="col-full">
+    <?php } ?>
+
+
+
+    <?php
+    /**
+     * Functions hooked in to storefront_footer action
+     *
+     * @hooked storefront_footer_widgets - 10
+     * @hooked storefront_credit         - 20
+     */
+    do_action('storefront_footer');
+    ?>
+
+    <?php if (!is_checkout()) { ?>
+
+    </div><!-- .col-full -->
+  </footer><!-- #colophon -->
+<?php } ?>
+
+
+
+<?php do_action('storefront_after_footer'); ?>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
