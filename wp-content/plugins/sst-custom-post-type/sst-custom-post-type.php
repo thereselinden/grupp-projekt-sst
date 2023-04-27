4bb252dcbf69809a86c344bba38d9810a0d967b5
Therese Linden <theresemlinden@gmail.com>
2023-04-27 13:43:17 +0200

    changed label for CPT

--- wp-content/plugins/sst-custom-post-type/sst-custom-post-type.php
+++ wp-content/plugins/sst-custom-post-type/sst-custom-post-type.php
@@ -7,16 +7,20 @@
 ?>
 
 <?php add_action('init', 'SST_register_CPT_store') ?>
-
-
 <?php function SST_register_CPT_store()
 {
+  // $labels = [
+  //   'name' => 'Butiker',
+  //   'single_name' => 'Butik'
+  // ];
+
   $store_args = array(
     'public' => true,
-    'label' => 'Butik',
+    'label' => 'Butiker',
     'show_in_rest' => true,
     'supports' => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'),
+    'has_archive' => true, //säg att vi har en arkiv sida
   );
-  register_post_type('store', $store_args);
+  register_post_type('butik', $store_args);
 }
 ?>
